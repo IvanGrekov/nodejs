71ebb27993bbee314d72d9f7c027685aee483f08
Ivan Grekov <ivan.grekov@windmill.digital>
2022-10-02 18:04:54 +0300

    require in type: module project

--- modules/import-export-packageJson/app.js
+++ modules/import-export-packageJson/app.js
@@ -1,5 +1,6 @@
 import { add, subtract } from './actions.js';
 import { number } from './number.js';
+// const { number } = require('./number.js'); // NOTE: require is not defined in ES module scope
 
 console.log('number', number);
 
