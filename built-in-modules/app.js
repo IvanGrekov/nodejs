ffce5aaf4353bb35ddc436ce647baa5d81eb674c
Ivan Grekov <ivan.grekov@windmill.digital>
2023-01-02 15:32:18 +0200

    differences between path.resolve and require.resolve and require.resolve.paths

--- built-in-modules/app.js
+++ built-in-modules/app.js
@@ -4,3 +4,4 @@ const path = require('path');
 console.log(os.version());
 console.log(path.resolve('package,json'));
 // console.log(require.resolve('package,json'));
+console.log(require.resolve.paths('package,json'));
